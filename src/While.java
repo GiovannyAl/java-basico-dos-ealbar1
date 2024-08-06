99aae91a7fbb21cb6e83fa9faaadb301f28f884d
GiovannyAl <137365473+GiovannyAl@users.noreply.github.com>
2024-08-06 08:18:09 -0500

    Primera version

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            suma += i;
+        }
+        System.out.println("La sumatoria de los números entre 1 y 10 es: " + suma);
+    }
+}
